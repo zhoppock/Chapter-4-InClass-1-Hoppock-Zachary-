7aaeae3d6292c8bc56fa3fce239d92fdae317755
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-28 01:20:16 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Hoppock, Zachary
+// forLoop.cpp
+// 10/8/2020
+// Program will do a for loop on how many numbers the variable "i" is
+// Version # 1
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+  for (int i = 0; i <= 4; i++)
+    cout << "i is " << i << endl;
+  return 0;
+}
